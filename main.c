faa7d962be2cc3d986bf9182ab1ae325deee73d5
Urja Rannikko <urjaman@gmail.com>
2017-03-22 17:13:05 +0200

    WDT-based sleepy timer stuff tested

--- main.c
+++ main.c
@@ -1,13 +1,40 @@
 #include "main.h"
 #include <avr/pgmspace.h>
 
+
+static volatile uint8_t wdt_ticker = 0;
+
+ISR(WDT_vect)
+{
+	wdt_ticker++;
+}
+
+void wdt_init(void) {
+	/* This sequence both turns off WDT reset mode and sets the period to 32ms */
+	/* Thus preparing WDT to be used as a simple timer later. */
+	wdt_reset();
+	WDTCR = _BV(WDCE) | _BV(WDE);
+	WDTCR = _BV(WDIF) | _BV(WDIE) | _BV(WDP0);
+}
+
+void wdt_delay(uint8_t ticks) {
+	if (!ticks) return; /* This would have unreliable 0 or 256 behaviour... */
+	cli();
+	uint8_t end = wdt_ticker + ticks;
+	do {
+		sys_sleep(SLEEP_MODE_PWR_DOWN);
+	} while (wdt_ticker != end);
+	return;
+}
+
 void sys_sleep(uint8_t mode)
 {
 	uint8_t cr = MCUCR & 0xC7; // Clear sleep-related bits
 	uint8_t cre = cr | _BV(SE);
 	cli();
 	// Clear sets mode = idle
-	if (!TCCR0B) cre |= mode; // Idle needed if TCCR0B to let timer run
+	//if (!TCCR0B) 
+	cre |= mode; // Idle needed if TCCR0B to let timer run
 	MCUCR = cre; /* sleep enable and set mode */
 	sei();
 	sleep_cpu();
@@ -27,27 +54,22 @@ void hex_out(uint8_t d) {
 //	USI_UART_Transmit_Byte(' ');
 }
 
-uint8_t duhdata = 0x54;
-
-
-const char stringy[] PROGMEM __attribute__((used)) = "OMG";
 
 void main(void) {
 	CLKPR = _BV(CLKPCE);
 	CLKPR = 0;
 	PORTB = 0x0F;
 	DDRB  = 0x07;
+	wdt_init();
 	sei();
 	for (;;) {
-		duhdata++;
 		PORTB &= ~_BV(2);
-		PORTB |= _BV(0);
-		PORTB |= _BV(1);
-		_delay_ms(500);
-		PORTB |= _BV(2);
 		PORTB &= ~_BV(0);
+		PORTB |= _BV(1);
+		wdt_delay(16);
+		PORTB |= _BV(2);
+		PORTB |= _BV(0);
 		PORTB &= ~_BV(1);
-		_delay_ms(500);
-		if (duhdata == 0) _delay_ms(1);
+		wdt_delay(16);
 	}
 }
