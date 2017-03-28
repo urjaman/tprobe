96b094aa35a8039c76809755addb3019f4575837
Urja Rannikko <urjaman@gmail.com>
2017-03-28 17:23:55 +0300

    get rid of the stdio :P
    other prep too...

--- main.c
+++ main.c
@@ -3,15 +3,46 @@
 #include "probeuart.h"
 #include "uart.h"
 #include "adc.h"
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-static int mputch(char c, FILE *stream);
-static FILE mstdout = FDEV_SETUP_STREAM(mputch, NULL, _FDEV_SETUP_WRITE);
+static void ss_P(PGM_P str) {
+	for(;;) {
+		uint8_t val = pgm_read_byte(str);
+		if (val) uart_tx(val);
+		else break;
+		str++;
+	}
+}
 
-static int mputch(char c, FILE *stream) {
-	if (c == '\n') mputch('\r', stream);
-	uart_tx(c);
-	return 0;
+static void ss(const unsigned char * str) {
+	for(;;) {
+		uint8_t val = *str;
+		if (val) uart_tx(val);
+		else break;
+		str++;
+	}
+}
+
+static void u0x(uint16_t v, uint8_t x) {
+	uint8_t tb[6];
+	utoa(v, (char*)tb, 10);
+	uint8_t l = strlen((char*)tb);
+	while (l<x) { uart_tx('0'); l++; }
+	ss(tb);
+}
+
+
+static uint8_t hex_char(uint8_t nib) {
+	nib &= 0xF;
+	nib |= 0x30;
+	if (nib > 0x39) nib += 7;
+	return nib;
+}
+
+static void X02(uint8_t d) {
+	uart_tx(hex_char(d>>4));
+	uart_tx(hex_char(d));
 }
 
 
@@ -47,23 +78,49 @@ void sys_sleep(uint8_t mode)
 	MCUCR = cr; /* sleep_disable */
 }
 
-static uint8_t hex_char(uint8_t nib) {
-	nib &= 0xF;
-	nib |= 0x30;
-	if (nib > 0x39) nib += 7;
-	return nib;
-}
 
-void hex_out(uint8_t d) {
-	uart_tx(hex_char(d>>4));
-	uart_tx(hex_char(d));
-	uart_tx(' ');
-}
 
 uint8_t prb_char(void) {
 	if (!(FLREG&_BV(PROBE_Z))) return '0' + (FLREG&_BV(PROBE_V));
 	return 'Z';
 }
+
+void probe_off(void) {
+	FLREG |= _BV(PROBE_OFF);
+}
+
+void probe_on(void) {
+	if (FLREG & _BV(PROBE_OFF)) {
+		cli();
+		FLREG &= ~_BV(PROBE_OFF);
+		probe_check();
+		sei();
+	}
+}
+
+struct settings {
+	uint8_t m;
+	uint8_t l;
+	uint8_t altfn;
+	uint8_t c;
+} s;
+
+void settings_load(void) {
+//	struct settings chk;
+//	const uint8_t * const crc_rp = (uint8_t*)&chk;
+	/* Load defaults */
+	s.altfn = 1;
+
+}
+
+#if 0
+void settings_save(void) {
+	const uint8_t * const crc_rp = (uint8_t*)&s;
+	s.m = 0xA7;
+	s.l = sizeof(struct settings);
+
+}
+#endif
 
 void main(void) {
 	CLKPR = _BV(CLKPCE);
@@ -75,13 +132,30 @@ void main(void) {
 	GIFR |= _BV(PCIF);
 	GIMSK = _BV(PCIE);
 	sei();
-	stdout = &mstdout;
+	settings_load();
 	uint8_t ntick;
 	for (;;) {
 		uint8_t c1 = prb_char();
-		uint8_t c2 = 0;
-		if (uart_rx_bytes()) c2 = uart_rx();
-		printf("\r%c: %04d/%04d mV; IO=%02X ZU:%d ZD:%d", c1, adc_sample_probe_mV(), adc_get_vcc_mV(), c2, pb_zup, pb_zdn);
+		if (uart_rx_bytes()) {
+			uint8_t c = uart_rx();
+			if ((c>='1')&&(c<='7')) s.altfn = (c - '0') | 0x80;
+			if (c=='0') s.altfn &= ~0x80;
+		}
+		uint8_t b = BUTTON;
+		if (!b) {
+			uart_tx('\r');
+			uart_tx(c1);
+			ss_P(PSTR(": "));
+			u0x(adc_sample_probe_mV(), 4);
+			uart_tx('/');
+			u0x(adc_get_vcc_mV(), 4);
+			ss_P(PSTR(" mV; A="));
+			X02(s.altfn);
+			ss_P(PSTR(" ZU:"));
+			u0x(pb_zup, 0);
+			ss_P(PSTR(" ZD:"));
+			u0x(pb_zdn, 2);
+		}
 		ntick = wdt_ticker+32;
 		do {
 			sys_sleep(SLEEP_MODE_PWR_DOWN);
