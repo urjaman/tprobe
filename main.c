c13def35c20af6e6fdd8f47f741b31f6c3298fcf
Urja Rannikko <urjaman@gmail.com>
2018-04-11 12:33:16 +0000

    Implement supertimer, altmode timeout and some test-like modes

--- main.c
+++ main.c
@@ -6,6 +6,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+static uint16_t stimer_hi = 0;
+static uint8_t stimer_ll = 0;
+
+/* Uses the wdt ticker to generate a higher-width counter, and returns result of that counter. */
+/* Needs to be called often enough (more often than every 4s or so) not to miss an overflow of the wdt ticker. */
+static uint24_t supertimer(void) {
+	uint8_t n = wdt_ticker;
+	if (n < stimer_ll) stimer_hi++;
+	stimer_ll = n;
+	return ((uint24_t)stimer_hi << 8) | stimer_ll;
+}
+
+
 static void ss_P(PGM_P str) {
 	for(;;) {
 		uint8_t val = pgm_read_byte(str);
@@ -144,6 +157,38 @@ void show_val(uint8_t v, uint8_t d) {
 	wdt_delay(d);
 }
 
+static uint24_t altmode_timebase;
+
+void enter_altmode(void) {
+	s.altfn |= 0x80;
+	probe_off();
+	altmode_timebase = supertimer();
+}
+
+void set_enter_altmode(uint8_t m) {
+	s.altfn = m & 7;
+	enter_altmode();
+}
+
+void exit_altmode(void) {
+	s.altfn &= ~0x80;
+	show_val(0,0);
+	probe_on();
+}
+
+uint8_t altmode_check(void) {
+	if (!(s.altfn & 0x80)) return 0;
+	if (s.altfn == 0x87) return 1; /* No timeout for mode 7. */
+	uint24_t timeout = 2*60*60*32UL; /* Default altmode timeout 2 hours. */
+	if (s.altfn == 0x86) timeout = 5*32; /* Mode 6 only 5 sec */
+	uint24_t passed = supertimer() - altmode_timebase;
+	if (passed >= timeout) {
+		exit_altmode();
+		return 0;
+	}
+	return 1;
+}
+
 /* Button (-down transition) state tracker */
 static uint8_t bdr = 0;
 static uint8_t bdw = 0;
@@ -155,7 +200,8 @@ static uint8_t bdo_since(uint8_t off) {
 	return passed;
 }
 
-static void altfn_transition(void) {
+static void altfn_transition(uint8_t m) {
+	set_enter_altmode(m);
 	show_val(7,5);
 	show_val(0,4);
 	show_val(s.altfn,10);
@@ -185,14 +231,24 @@ void tri_tap_menu(void) {
 		} else if ((b)&&(butt >= 0)) {
 			uint8_t bp = wdt_ticker - butt;
 			if (bp > 32) {
-				s.altfn = 0x80 | mfn;
 				show_val(0,10);
-				altfn_transition();
+				altfn_transition(mfn);
 				return;
 			}
 		}
 	} while (1);
 }
+
+uint8_t vcc_lipo_dispval(uint16_t vcc) {
+	if (vcc >= 4350) return 7; /* Ext. 4.5+ */
+	if (vcc >= 3950) return 6; /* 4.0V+ Battery */
+	if (vcc >= 3650) return 5; /* 3.7V+ Battery */
+	if (vcc >= 3400) return 4; /* 3.5V ish */
+	if (vcc >= 3200) return 3; /* 3.3V ish */
+	if (vcc >= 2950) return 2; /* 3.0V ish */
+	return 1; /* We're still alive... */
+}
+
 
 void main(void) {
 	CLKPR = _BV(CLKPCE);
@@ -212,12 +268,10 @@ void main(void) {
 		if (uart_rx_bytes()) {
 			uint8_t c = uart_rx();
 			if ((c>='1')&&(c<='7')) {
-				probe_off();
-				s.altfn = (c - '0') | 0x80;
+				set_enter_altmode(c);
 			}
 			if (c=='0') {
-				probe_on();
-				s.altfn &= ~0x80;
+				exit_altmode();
 			}
 
 		}
@@ -234,14 +288,11 @@ void main(void) {
 		if (bdw != bdr) {
 			if (b) {
 				if (s.altfn & 0x80) {
-					probe_on();
+					exit_altmode();
 					bdw = bdr;
-					s.altfn &= ~0x80;
 				} else if (bdo_since(bdw-1) >= 32) {
-					probe_off();
-					s.altfn |= 0x80;
 					bdw = bdr;
-					altfn_transition();
+					altfn_transition(s.altfn);
 				}
 			}
 			if (bdo_since(bdr) >= 128) { /* forget stuff before it's so old it confuses us. */
@@ -257,20 +308,22 @@ void main(void) {
 				}
 			}
 		}
+		uint16_t probe = adc_sample_probe_mV();
+		uint16_t vcc = adc_get_vcc_mV();
 
-		if (s.altfn & 0x80) {
+		if (altmode_check()) {
 			/* alt. fn active */
 			if (s.altfn == 0x87) { /* special deep sleep mode ... */
 				if (!b) { /* make sure the button is not pressed when we do it */
 					deep_sleep();
 				}
+			} else if (s.altfn == 0x86) {
+				show_val(vcc_lipo_dispval(vcc), 0);
 			}
 		} else {
 
 		}
 
-		uint16_t probe = adc_sample_probe_mV();
-		uint16_t vcc = adc_get_vcc_mV();
 		if ((!b)&&(vcc>=4200)) { /* toggle UART usage based on power source present. */
 			uart_tx('\r');
 			uart_tx(c1);
--- main.h
+++ main.h
@@ -9,3 +9,6 @@
 void sys_sleep(uint8_t mode);
 
 extern uint8_t boot_sr;
+
+typedef __int24 int24_t;
+typedef __uint24 uint24_t;
